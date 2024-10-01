5a6b183f2ba846429ba05230ae3cfb9a2f7cb8fb
James Lee <jamesusa19@yahoo.com>
2024-09-30 23:02:24 -0700

    Finished go API

--- api/api.go
+++ api/api.go
@@ -1,5 +1,10 @@
 package api
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 type CoinBalanceParams struct {
 	Username string
 }
@@ -7,7 +12,7 @@ type CoinBalanceParams struct {
 type CoinBalanceResponse struct {
 	//Status code (ex: 200)
 	Code    int
-	Balance int64
+	Balance uint64
 }
 
 type Error struct {
@@ -15,3 +20,24 @@ type Error struct {
 	Code    int
 	Message string
 }
+
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code:    code,
+		Message: message,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+var (
+	RequestErrorHandler = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		writeError(w, "An unexpected error occurred", http.StatusInternalServerError)
+	}
+)
